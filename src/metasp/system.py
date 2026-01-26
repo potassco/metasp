bcf7b1eca9c16cedf50aac0a9b6caf99c301fab2
Amade Nemes <nemesamade@gmail.com>
2026-01-26 16:26:09 +0100

    Implement fo transform for mel and del.

--- src/metasp/system.py
+++ src/metasp/system.py
@@ -78,6 +78,13 @@ class MetaSystem:
         self.python_scripts = python_scripts or []
         self._set_printing_function(print_model)
 
+        if not self.semantics_encoding:
+            log.error("No semantics encoding files specified.")
+            raise ValueError("No semantics encoding files specified.")
+        out_dir = os.path.join(os.path.dirname(os.path.abspath(self.semantics_encoding[0])), "out")
+        os.makedirs(out_dir, exist_ok=True)
+        self.out_dir = out_dir
+
     @classmethod
     def from_dict(cls, config: dict) -> "MetaSystem":
         """
@@ -110,7 +117,7 @@ class MetaSystem:
             files (List[str]): The list of file paths to process.
             prg (str): The program string to process.
         """
-        out_dir = Path(self.get_out_dir())
+        out_dir = Path(self.out_dir)
         tree = AspenTree(default_language=clingo_lang)
 
         syntax_enc_symbols = [tree.parse(Path(e)) for e in self.syntax_encoding]
@@ -136,6 +143,13 @@ class MetaSystem:
         str_input_source = tree.sources[str_input_symb]
         rewritten_program_str += str(str_input_source.source_bytes, encoding=str_input_source.encoding)
 
+        if len(files) > 0:
+            fname = "_".join([Path(f).stem for f in files]) + "_stdin.lp"
+        else:
+            fname = "stdin.lp"
+        with open(out_dir / fname, "w") as f:
+            f.write(rewritten_program_str)
+
         tree = AspenTree(default_language=clingo_lang)
         semantic_enc_symbols = [tree.parse(Path(e)) for e in self.semantics_encoding]
         tree.transform(
@@ -153,7 +167,6 @@ class MetaSystem:
                 sem_file.write(str(source.source_bytes, encoding=source.encoding))
             semantics_encoding.append(str(out_file))
         self.semantics_encoding = semantics_encoding
-        # print(rewritten_program_str)
         return rewritten_program_str
 
     def _replace_package_includes(self, file: str) -> str:
@@ -225,23 +238,6 @@ class MetaSystem:
                 raise ValueError(f"You must provide the constant  {const} to run the system, using -c {const}=<val>.")
         self.constants = constants.copy()
 
-    def get_out_dir(self) -> str:
-        """Get the output directory for the system output files.
-
-        TODO: This generates issues when we want to have include statements in the semantics encoding because we must consider the nesting.
-        Raises:
-            ValueError: If no semantics encoding files are specified.
-
-        Returns:
-            str: The output directory path.
-        """
-        if not self.semantics_encoding:
-            log.error("No semantics encoding files specified.")
-            raise ValueError("No semantics encoding files specified.")
-        out_dir = os.path.join(os.path.dirname(os.path.abspath(self.semantics_encoding[0])), "out")
-        os.makedirs(out_dir, exist_ok=True)
-        return out_dir
-
     def get_files(self, reified_input: str) -> Sequence[str]:
         """
         Get the list of files to be used for the system.
@@ -254,8 +250,7 @@ class MetaSystem:
         """
         semantics_with_includes = "\n".join([self._replace_package_includes(f) for f in self.semantics_encoding])
 
-        out_dir = self.get_out_dir()
-        tmp_file_path = os.path.join(out_dir, f"{self.name}_combined.lp")
+        tmp_file_path = os.path.join(self.out_dir, f"{self.name}_combined.lp")
         with open(tmp_file_path, "w") as tmp_file:
             tmp_file.write(semantics_with_includes)
             tmp_file.write("\n")
